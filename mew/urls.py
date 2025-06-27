71f6d549f0ab860ecb6f472fde44d90cc065a785
wenxiao Lv <2064747320@qq.com>
2025-06-27 18:38:03 +0800

    modift the urls.py

--- mew/urls.py
+++ mew/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('', lambda request: redirect('chat/', permanent=False)),
     path('chat/', include('chat.urls')),
     path('moments/', include('moments.urls')),
-    path('profile/', include('profile.urls')),
+    path('user_profile/', include('user_profile.urls')),
     path('admin/', admin.site.urls),
 
     path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
